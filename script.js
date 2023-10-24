946815cf183a2e52ac0418bd4fdde931ddd6dc20
rsaarinen <rasmus.saarinen@edu.hyria.fi>
2023-10-24 10:24:57 +0300

    array methods

--- script.js
+++ script.js
@@ -49,10 +49,65 @@ else {
 }
 
 /*
-1. Tehkää silmukka, joka käy läpi luvut 1-100 ja tulostaa luvun jos se on jaollinen seitsemällä
+8. Tehkää silmukka, joka käy läpi luvut 1-100 ja tulostaa luvun jos se on jaollinen seitsemällä
 */
 for (let i = 1; i < 101; i++) {
     if (i % 7 == 0) {
         console.log(i)
     }
 }
+/*
+0. Muodosta lista, joka on 20 elementin pituinen ja 
+    sisältää satunnaisia kokonaislukuja väliltä 1 - 500.
+1. Poista listasta luvut, jotka ovat pienemiä kuin 10 ja suurempia kuin 30 ja tulosta se.
+2. Plussaa listan joka elementtiin 5 ja tulosta lista
+3. Etsi ensimmäinen luku joka on > 20
+4. Laita lista suuruusjärjestykseen
+5. Tee funktio, joka tarkistaa onko sille annettu string parametri palidromi 
+(eli onko sana lopusta alkuun sama kuin alusta loppuun. 
+    Esim "saippuakauppias" on palindromi ja "otto" on palindromi)
+*/
+let list = [8, 47, 16, 91, 75, 2, 22, 36, 17, 63];
+
+// 0.
+function getRandInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+let randList = [];
+for (let i = 1; i < 21; i++) {
+    randList.push(getRandInt(1, 500));
+}
+console.log(randList);
+
+// 1.
+function removeNum(list) {
+    return list.filter(function(num) {
+        return num > 10 && num < 30;
+    });
+}
+let removeNumList = removeNum(list)
+console.log(removeNumList)
+
+// 2.
+let plusFive = list.map((x) => x + 5);
+console.log(plusFive)
+
+// 3.
+function biggerThanTwenty(value) {
+    return value > 20;
+}
+let biggerThanTwentyList = list.filter(biggerThanTwenty);
+console.log(biggerThanTwentyList)
+
+// 4.
+let sortedList = list.sort(function(a, b){return a - b});
+console.log(sortedList)
+
+// 5.
+// palauttaa "true" jos on palindromi ja "false" jos ei
+function checkPalindrome(str) {
+    return str == str.split('').reverse().join('');
+}
+console.log(checkPalindrome("saippuakauppias"))
