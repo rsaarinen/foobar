59e9c45e2e75caecddeac1386268b2bf7d8fcaf7
rsaarinen <rasmus.saarinen@edu.hyria.fi>
2023-10-27 12:42:29 +0300

    4 ja 5

--- teaching/script.js
+++ teaching/script.js
@@ -26,3 +26,23 @@ document.body.addEventListener("mousemove", function (a) {
   divElement.innerText = coords;
   console.log(coords);
 });
+
+// 4.
+let teksti1 = document.getElementsByClassName("teksti")[0];
+let teksti2 = document.getElementsByClassName("teksti")[1];
+let teksti3 = document.getElementsByClassName("teksti")[2];
+function changeColor(event) {
+  event.target.style.color = "red";
+}
+teksti1.addEventListener("click", changeColor);
+teksti2.addEventListener("click", changeColor);
+teksti3.addEventListener("click", changeColor);
+
+// 5.
+elaimet = ["hirvi", "karhu", "orava", "käärme", "apina", "norsu", "hämähäkki"];
+lista = document.getElementById("animals");
+for (let i = 0; i < elaimet.length; i++) {
+  const li = document.createElement("li");
+  li.innerHTML = elaimet[i];
+  lista.appendChild(li);
+}
